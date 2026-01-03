dda98d7b5c9674fae2127defa82fe5e3d29eddd1
Shreedhar Chaudhary <chaudharyshreedhar78@gmail.com>
2026-01-03 10:03:55 +0545

    feat(auth): add user model and repository

---
+++ backend/src/modules/auth/infrastructure/user.repository.js
@@ -0,0 +1,11 @@
+﻿import { UserModel } from "./user.model.js";
+
+const findByEmail = (email) => UserModel.findOne({ email }).exec();
+const createUser = (data) => UserModel.create(data);
+const findById = (id) => UserModel.findById(id).exec();
+
+export const userRepository = {
+  findByEmail,
+  createUser,
+  findById
+};
